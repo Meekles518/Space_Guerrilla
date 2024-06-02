15489d1f7f8e263f5193647243bcff7f8157ba82
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-06-03 00:38:38 +0900

    재장전 및 장전 탄환 표시 로직 구현 중
    재장전 시간을 표시하는 원 및 현재 장전된 탄환 수를 표시하는 UI 작업 중. 장전 탄환 수를 표시하는 함수는 구현 완료 했으나, 재장전 로직에서 문제가 있음을 발견. 원하는 재장전은 한 발씩 차곡차곡 브롤스타즈처럼 장전되는 것인데, 현재 재장전 로직은 한 번에 최대 장전수까지 탄환을 충전해버리는 방식이다. 재장전 로직에 연관된 함수들을 전부 수정함과 동시에 최적화하는 과정이 우선시되어야 한다.

--- Space_Guerrilla/Assets/Scripts/Field/UI/Player_UI.cs
+++ Space_Guerrilla/Assets/Scripts/Field/UI/Player_UI.cs
@@ -18,21 +18,91 @@ public class Player_UI : MonoBehaviour
 
     public int magCpapcity; //전체 탄창 크기 저장할 변수
     public int magAmmo; //현재 장전 탄환 크기 저장할 변수
+    public int activeShooter; //현재 활성화된 Shooter의 수
 
-
+    public PlayerShooter[] shooters; //PlayerShooter들을 저장할 배열
     
+
+
+    public float lastreloadtime; //마지막 장전 시점
+    public float reloadtime; //재장전에 필요한 시간을 저장
+
+
+    public void Awake()
+    {
+        //UI 조정에 필요한 Component 가져오기
+        circle = ReloadCircle.GetComponent<Image>();
+        txt = BulletCnt.GetComponent<TextMeshProUGUI>();
+    }
 
 
 
     public void FixedUpdate()
     {
 
+        shooters = GameManager.instance.player.GetComponents<PlayerShooter>();
+        magUpdate();
+        reloadCircleUpadte();
+
     }
 
+    //현재 탄환 수를 표시하는 UI를 조정하는 함수
+    public void magUpdate()
+    {
+        //매 주기마다 전체 탄창 크기와 현재 장전 탄환 크기를 초기화
+        magCpapcity = 0;
+        magAmmo = 0;
+        activeShooter = 0;
 
 
+        foreach (var pShooter in shooters)
+        {
+            //Shooter의 magCapacity, 최대 총알이 0 이하라면 검색할 필요가 없으므로 건너뛰기.
+            if (pShooter.shooter.magCapacity <= 0)
+            {
+                continue;
+            }
+
+            magCpapcity += pShooter.shooter.magCapacity;
+            magAmmo += pShooter.shooter.magAmmo;
+            activeShooter++;
+
+        }
+
+        if(activeShooter > 0)
+        {
+            //txt 변경 해주기
+            txt.text = $"{magAmmo / activeShooter}/{magCpapcity / activeShooter}";
+        }
+
+        else
+        {
+            txt.text = "0/0";
+        }
+         
+
+    }
+
+    //재장전에 필요한 시간을 원으로 표현하는 UI를 조정하는 함수
+    public void reloadCircleUpadte()
+    {
+        foreach (var pShooter in shooters)
+        {
+            //pShooter의 최대 탄창이 0이상이고, PlayerShooter에서 재장전을 시행한다면
+            if (pShooter.shooter.magCapacity > 0 && pShooter.reloadCheck())
+            {
+
+                lastreloadtime = Time.time; //현재 시간 저장
 
 
+                break;
+            }
+        }
+
+
+        circle.fillAmount = 0;
+
+    }
 
 
 
