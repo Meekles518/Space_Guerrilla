c4739f488b7603b8df60d201ab8dd5cfe3877762
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-07-03 20:18:02 +0900

    Aegis 스킬 기능 구현 중
    우선 애프터버너를 실행시키는 코루틴 작성중. 이후에는 스트림 라이너 작성 예정

--- Space_Guerrilla/Assets/Scripts/Field/Player/FieldSkill/AegisSkill.cs
+++ Space_Guerrilla/Assets/Scripts/Field/Player/FieldSkill/AegisSkill.cs
@@ -9,7 +9,7 @@ namespace Skill
 
         private PlayerInput playerInput; // PlayerInput을 불러옴
         private PlayerMovement playerMovement;
-        private PlayerShooter[] shooters; //PlayerShooter들을 저장할 배열
+        private Shooter[] shooters; //PlayerShooter들을 저장할 배열
 
 
         //skill의 이름들을 담을 리스트
@@ -42,6 +42,8 @@ namespace Skill
         public int afterBurnerUse;
         public float afterBurnerTime = 2.5f;
         public float afterBurnerSpeed = 50f;
+        public bool afterBurnerUsing = false;
+        public int magCapacity;
 
         //각각 스킬 가능 횟수, 스킬 사용한 횟수, 스킬 지속 시간, 주 무기 과열 시간 수치
         public float streamLinerCnt = 1;
@@ -61,6 +63,11 @@ namespace Skill
             afterBurnerUse = 0;
             streamLinerUse = 0;
 
+            //player의 component들 가져오기
+            playerInput = GetComponent<PlayerInput>();
+            playerMovement = GetComponent<PlayerMovement>();
+            shooters = GetComponentsInChildren<Shooter>();
+            magCapacity = shooters[0].magCapacity;
         }
 
 
@@ -70,7 +77,81 @@ namespace Skill
 
         }
 
+        //이아래에 스킬 함수들 구현 시작
 
+
+        IEnumerator Aegis_afterBurner(float time)
+        {
+            //사용 횟수가 남아있다면
+            if (afterBurnerCnt > afterBurnerUse)
+            {
+                
+                //우선 스킬 사용중인지를 체크
+                if (!afterBurnerUsing)
+                {
+                    
+                    afterBurnerUsing = true; //사용중 표시
+                    playerMovement.moveSpeed += afterBurnerSpeed; //이동속도 증가
+
+                    //모든 shooter의 최대 탄환과 현재 탄환 수를 0으로 만들기
+                    for (int i = 0; i < shooters.Length(); i++)
+                    {
+                        shooters[i].magCapacity = 0;
+                        shooters[i].magAmmo = 0;
+                    }
+
+                    yield return new WaitForSeconds(time);
+
+                    //스킬의 효과가 지속되고 있을 경우에만 아래 코드 실행
+                    if (afterBurnerUsing)
+                    {
+                        playerMovement.moveSpeed -= afterBurnerSpeed;
+
+                        //모든 shooter의 최대 탄환과 현재 탄환 수를 최대로 만들기
+                        for (int i = 0; i < shooters.Length(); i++)
+                        {
+                            shooters[i].magCapacity = magCapacity;
+                            shooters[i].magAmmo = magCapacity;
+                        }
+
+                        afterBurnerUse = false; //사용 안함 표시
+                        afterBurnerUse++; //사용한 횟수 증가
+                    }
+
+                     
+
+                }
+
+                //스킬 효과 시전 중 다시 스킬 버튼을 눌러 중도에 취소 할 경우에
+                else
+                {
+                    //스킬의 효과가 지속되고 있을 경우에만 아래 코드 실행
+                    if (afterBurnerUsing)
+                    {
+                        playerMovement.moveSpeed -= afterBurnerSpeed;
+
+                        //모든 shooter의 최대 탄환과 현재 탄환 수를 최대로 만들기
+                        for (int i = 0; i < shooters.Length(); i++)
+                        {
+                            shooters[i].magCapacity = magCapacity;
+                            shooters[i].magAmmo = magCapacity;
+                        }
+
+                        afterBurnerUse = false; //사용 안함 표시
+                        afterBurnerUse++; //사용한 횟수 증가
+                    }
+                }
+
+            }
+
+
+        }
+
+         
+        public void Aegis_streamLiner()
+        {
+
+        }
 
     }
 }
