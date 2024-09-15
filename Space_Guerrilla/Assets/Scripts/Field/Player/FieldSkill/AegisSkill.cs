3086c36510088d4ef6f583b74ed16ac5283cbc6d
Kang-conqueror <hik8835@daum.net>
2024-09-16 00:55:42 +0900

    add launchCruiseMissile, fix Aegis_afterBurner, Aegis_streamLiner
    미사일 기능 구현, 스킬 사용 여부 변수 통일화(동시에 여러 스킬 사용 불가). 최적화는 나중에 하고 기능 구현 우선으로 코드 작성했음.

--- Space_Guerrilla/Assets/Scripts/Field/Player/FieldSkill/AegisSkill.cs
+++ Space_Guerrilla/Assets/Scripts/Field/Player/FieldSkill/AegisSkill.cs
@@ -43,7 +43,6 @@ namespace Skill
         public float afterBurnerTime = 2.5f;
         public float afterBurnerSpeed = 50f;
         public bool afterBurnerUsing = false; // 스킬 효과(속도 증가) 작동중 여부
-        public bool afterBurnerAvailable = true; // 스킬 사용 가능 여부
         public int magCapacity;
 
         //각각 스킬 가능 횟수, 스킬 사용한 횟수, 스킬 지속 시간, 주 무기 과열 시간 수치
@@ -52,9 +51,15 @@ namespace Skill
         public float streamLinerTime = 3f;
         public float streamLinerCool = 2f;
         public bool streamLinerUsing = false; // 무한 탄환 작동중 여부
-        public bool streamLinerAvailable = false; // 스킬 사용 가능 여부
         public int streamLinerMax = 999;
         public float streamLinerRotateTime = 1.5f;
+
+
+        //각각 스킬 가능 횟수, 스킬을 사용한 횟수
+        public float cruiseMissileCnt = 1;
+        public int cruiseMissileUse = 0;
+
+        public bool isSkillActive = false; //현재 동작중인 스킬이 있는지 여부
 
 
         //미사일 발사 스킬에 관한 변수들
@@ -81,6 +86,22 @@ namespace Skill
         // Update is called once per frame
         private void FixedUpdate()
         {
+            //임시로 Q, Z, X 키에 각각 애프터버너, 스트림라이너, 미사일 스킬 구현
+
+            if (playerInput.skillQ)
+            {
+                StartCoroutine("Aegis_afterBurner");
+            }
+
+            if (playerInput.skillZ)
+            {
+                StartCoroutine("Aegis_streamLiner");
+            }
+
+            if (playerInput.skillX)
+            {
+                launchCruiseMissile();
+            }
 
         }
 
@@ -93,7 +114,7 @@ namespace Skill
         public IEnumerator Aegis_afterBurner()
         {
             //사용 횟수가 남아있다면
-            if (afterBurnerCnt > afterBurnerUse)
+            if (afterBurnerCnt > afterBurnerUse && !isSkillActive)
             {
                 
                 //우선 스킬 사용중인지를 체크
@@ -123,8 +144,10 @@ namespace Skill
         {
             if (!afterBurnerUsing)
             {
+                Debug.Log("afterBurnerActive");
+
+                isSkillActive = true; //스킬 사용중 표시
                 afterBurnerUsing = true; //사용중 표시
-                streamLinerAvailable = false; // 스트림라이너 사용 불가 표시
                 streamLinerCancel(); // 스트림라이너 스킬 기능 중단(기능 작동중일 때에만 실행되는 함수)
                 playerMovement.moveSpeed += afterBurnerSpeed; //이동속도 증가
 
@@ -141,6 +164,9 @@ namespace Skill
             //스킬의 효과가 지속되고 있을 경우에만 아래 코드 실행
             if (afterBurnerUsing)
             {
+
+                Debug.Log("afterBurnerCancel");
+
                 afterBurnerUsing = false; //사용 안함 표시
                 playerMovement.moveSpeed -= afterBurnerSpeed;
 
@@ -149,6 +175,8 @@ namespace Skill
 
                 
                 afterBurnerUse++; //사용한 횟수 증가
+
+                isSkillActive = false; //스킬 사용중이 아님 표시
 
                 //이후에 스킬 아이콘을 원래의 이미지로 변경하는 코드 필요
             }
@@ -162,6 +190,8 @@ namespace Skill
                 shooters[i].magCapacity = 0;
                 shooters[i].magAmmo = 0;
             }
+
+            Debug.Log("magnumZero");
         }
         private void magnumSet()
         {
@@ -177,13 +207,15 @@ namespace Skill
                 shooters[i].magCapacity = magCapacity;
                 shooters[i].magAmmo = magCapacity;
             }
+
+            Debug.Log("magnumSet");
         }
 
         
         public IEnumerator Aegis_streamLiner()
         {
             //스킬 사용 횟수가 남아있고, 스킬 사용중이 아니며 사용 가능하다면
-            if (streamLinerCnt > streamLinerUse && !streamLinerUsing && streamLinerAvailable)
+            if (streamLinerCnt > streamLinerUse && !streamLinerUsing && !isSkillActive)
             {
 
                 streamLinerActive(); // 스킬 실행
@@ -199,9 +231,12 @@ namespace Skill
         private void streamLinerActive()
         {
             //스킬 사용중이 아니고, 사용 가능하다면
-            if (!streamLinerUsing && streamLinerAvailable)
+            if (!streamLinerUsing && !isSkillActive)
             {
+                Debug.Log("streamLinerActive");
+
                 streamLinerUsing = true; //스킬 사용중 표시
+                isSkillActive = true; //스킬 사용중 표시
 
                 magnumZero(); // 회전 하는 동안 발사 못하게 탄환 제거
 
@@ -215,9 +250,11 @@ namespace Skill
         //shooter 회전 및 총알 수를 제어하는 Coroutine
         private IEnumerator rotateShooter(bool active)
         {
-
-            Quaternion rightShooter = shooters[0].transform.rotation;
-            Quaternion leftShooter = shooters[1].transform.rotation;
+            //0번이 우측, 1번이 좌측, 2번이 중앙 포대
+            //임시로 하드 코딩으로 각각 우측을 -30, 좌측을 30 의 z축 rotation 값으로 설정함.
+            Quaternion rightShooter = Quaternion.Euler(0, 0, -30f);
+            Quaternion leftShooter = Quaternion.Euler(0, 0, 30f);
+            Quaternion midShooter = Quaternion.Euler(0, 0, 0);
             float elapsedTime = 0f;
 
             //active가 true라면, 포신을 직선 방향으로 회전
@@ -226,14 +263,14 @@ namespace Skill
                 //streamLinerRotateTime 동안 shooter 들 회전시키기
                 while (elapsedTime < streamLinerRotateTime)
                 {
-                    shooters[0].transform.rotation = Quaternion.Lerp(rightShooter, Quaternion.Euler(0, 0, 0), elapsedTime / streamLinerRotateTime);
-                    shooters[1].transform.rotation = Quaternion.Lerp(leftShooter, Quaternion.Euler(0, 0, 0), elapsedTime / streamLinerRotateTime);
+                    shooters[0].transform.localRotation = Quaternion.Lerp(rightShooter, midShooter, elapsedTime / streamLinerRotateTime);
+                    shooters[1].transform.localRotation = Quaternion.Lerp(leftShooter, midShooter, elapsedTime / streamLinerRotateTime);
                     elapsedTime += Time.deltaTime;
                     yield return null;
                 }
                 //회전 후 오차값 방지 위해 목표 각도로 재 조정
-                shooters[0].transform.rotation = Quaternion.Euler(0, 0, 0);
-                shooters[1].transform.rotation = Quaternion.Euler(0, 0, 0);
+                shooters[0].transform.localRotation = midShooter;
+                shooters[1].transform.localRotation = midShooter;
 
                 //모든 shooter에 무한탄환 표시
                 for (int i = 0; i < shooters.Length; i++)
@@ -250,21 +287,21 @@ namespace Skill
                 //streamLinerRotateTime 동안 shooter 들 회전시키기
                 while (elapsedTime < streamLinerRotateTime)
                 {
-                    shooters[0].transform.rotation = Quaternion.Lerp(rightShooter, Quaternion.Euler(0, 0, -30f), elapsedTime / streamLinerRotateTime);
-                    shooters[1].transform.rotation = Quaternion.Lerp(leftShooter, Quaternion.Euler(0, 0, 30f), elapsedTime / streamLinerRotateTime);
+                    shooters[0].transform.localRotation = Quaternion.Lerp(midShooter, rightShooter, elapsedTime / streamLinerRotateTime);
+                    shooters[1].transform.localRotation = Quaternion.Lerp(midShooter, leftShooter, elapsedTime / streamLinerRotateTime);
                     elapsedTime += Time.deltaTime;
                     yield return null;
                 }
                 //회전 후 오차값 방지 위해 목표 각도로 재 조정
-                shooters[0].transform.rotation = Quaternion.Euler(0, 0, -30f);
-                shooters[1].transform.rotation = Quaternion.Euler(0, 0, 30f);
+                shooters[0].transform.localRotation = Quaternion.Euler(0, 0, -30f);
+                shooters[1].transform.localRotation = Quaternion.Euler(0, 0, 30f);
 
                 // 과부하 설정 시간 - 회전 소요 시간, 즉 남은 과부하 시간동안 대기
                 yield return new WaitForSeconds(streamLinerCool - streamLinerRotateTime);
 
                 //애프터버너 사용 중이 아니라면, 탄환 수를 정상으로 북구
                 magnumSet();
-                streamLinerAvailable = true; // 스킬 사용 가능 상태로 표시 
+                
             }
 
         }
@@ -274,13 +311,48 @@ namespace Skill
             //스킬 사용중이라면
             if (streamLinerUsing)
             {
+                Debug.Log("streamLinerCancel");
+
+                isSkillActive = false; // 스킬 사용 가능 상태로 표시 
                 streamLinerUse++; // 사용한 횟수 추가
                 streamLinerUsing = false; //사용 중 아님 표시
-                streamLinerAvailable = false; // 과부하로 사용 불가능함 표시
                 magnumZero(); // 탄환 0으로 만들기
                 StartCoroutine(rotateShooter(false)); // shooter 원래 방향으로 회전
             }
         }
+
+        private void launchCruiseMissile()
+        {
+            //다른 스킬이 동작중이라면 미사일 발사 불가
+            if (isSkillActive)
+            {
+                return;
+            }
+
+            if (cruiseMissileCnt <= cruiseMissileUse)
+            {
+                return;    
+            }
+
+            Debug.Log("launchCruiseMissile");
+
+            const int poolManagerPrefabIdx = 2; // Player_CruiseMissile이 저장된 idx, 이후 Pool Manager의 Prefabs 배열 변경 시 수정 필요
+
+            for (int i = 0; i < shooters.Length; i++)
+            {
+                // 각 shooter의 transform을 얻어옴
+                Transform shooterTransform = shooters[i].transform;
+
+                // PoolManager의 Get 메서드를 호출하여 미사일 생성
+                GameManager.instance.poolManager.Get(poolManagerPrefabIdx, shooterTransform);
+
+                // 미사일에 추가적인 설정이 필요하다면 여기서 설정
+                
+            }
+
+            cruiseMissileUse++;
+        }
+
 
     }
 }
